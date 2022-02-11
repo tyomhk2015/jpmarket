48c76298bbb2022bd5d340d80a0491c05a96eef1
tyomhk <cheertrain15@naver.com>
2022-02-11 23:16:29 +0900

    TailwindCSS on login page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
+import Enter from './enter';
 import Orders from './twcss/orders';
 
 const Home: NextPage = () => {
   return (
-    <Orders />
+    <Enter />
   );
 };
 
