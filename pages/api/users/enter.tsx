08c69998853a8245fbcc21cc312c5fb0bcce3f14
tyomhk <cheertrain15@naver.com>
2022-02-28 00:54:00 +0900

    Added 'Token' model and logics for getting / creating tokens.

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,18 +4,29 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 async function EnterAPIhandler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: 'anon',
-      ...payload,
-    },
-    update: {},
+  const userContact = phone ? { phone: +phone } : { email };
+  const payload = Date.now().toString(32);
+  const token = await client.token.create({
+    data: {
+      payload, // Token number.
+      user: {
+        connectOrCreate: {
+          // If there is a user with specified email/phone number,
+          // then connect the user to the token above.
+          where: {
+            ...userContact,
+          },
+          // If there is no user with specified email/phone number,
+          // create a user and connect the user to the token above.
+          create: {
+            name: 'anon',
+            ...userContact,
+          },
+        }
+      }
+    }
   });
-  console.log(user);
+  console.log(token);
   res.status(201).json({
     ok: true,
   });
