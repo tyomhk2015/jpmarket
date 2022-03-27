756e09f85009beb1a32c64450b933b842874f66d
tyomhk <cheertrain15@naver.com>
2022-03-28 01:52:51 +0900

    Tidied code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,33 @@
-import type { NextPage } from "next";
-import FloatingButton from "components/floating-button";
-import Item from "components/item";
-import Layout from "components/layout";
-import useSWR from "swr";
-import { Product } from "@prisma/client";
+import type { NextPage } from 'next';
+import FloatingButton from 'components/floating-button';
+import Item from 'components/item';
+import Layout from 'components/layout';
+import useSWR from 'swr';
+import { Product, User } from '@prisma/client';
+import { useEffect } from 'react';
 
 interface ProductResponse {
   ok: boolean;
   products: ProductResponseWithFav[];
+  user: number;
 }
 
 interface ProductResponseWithFav extends Product {
   _count: {
-    fav: number,
-  }
+    fav: number;
+  };
 }
 
-const Home: NextPage = () => {  const { data:productData } = useSWR<ProductResponse>("/api/products");
-  return (
-    <Layout title="홈" hasTabBar>
-      <div className="flex flex-col space-y-5 divide-y">
+const Home: NextPage = () => {
+  const { data: productData } = useSWR<ProductResponse>('/api/products');
 
-        {productData?.products.map((product) => (
+  useEffect(() => {
+    // When user is not logged in or in valid session, redirect the user to login the page.
+  }, [productData]);
+  return (
+    <Layout title='홈' hasTabBar>
+      <div className='flex flex-col space-y-5 divide-y'>
+        {productData?.products?.map((product) => (
           <Item
             id={product.id}
             key={product.id}
@@ -31,20 +37,20 @@ const Home: NextPage = () => {  const { data:productData } = useSWR<ProductRespo
             hearts={product._count.fav}
           />
         ))}
-        <FloatingButton href="/products/upload">
+        <FloatingButton href='/products/upload'>
           <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
+            className='h-6 w-6'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            aria-hidden='true'
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M12 6v6m0 0v6m0-6h6m-6 0H6'
             />
           </svg>
         </FloatingButton>
