4ce8d9ea84313aecd52f78af033dfe02e5e9030d
tyomhk <cheertrain15@naver.com>
2022-04-03 21:34:18 +0900

    Added margin for avatar component

--- components/message.tsx
+++ components/message.tsx
@@ -14,11 +14,11 @@ export default function Message({
   return (
     <div
       className={cls(
-        "flex  items-start",
+        "flex items-start",
         reversed ? "flex-row-reverse space-x-reverse" : "space-x-2"
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
+      <div className="w-8 h-8 mx-2 rounded-full bg-slate-400" />
       <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
         <p>{message}</p>
       </div>
