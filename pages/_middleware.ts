86c8843330ac6d080c5561831e5724875f22eae7
tyomhk <cheertrain15@naver.com>
2022-04-21 22:28:45 +0900

    Added redirecting logic for non-members.

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,9 @@
-import type { NextRequest, NextFetchEvent } from "next/server";
+import { read } from "fs";
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
 // Global Middleware of Next.js
 export function middleware(req: NextRequest, fe: NextFetchEvent) {
-  console.log(req.page);
+  if (!req.url.includes("/enter") && req.url.includes("/api") && !req.cookies) {
+    return NextResponse.redirect("/enter");
+  }
 }
